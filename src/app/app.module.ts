5fdc6cefedf0c21e56e93d0d255eab2ee9479a0f
Vi Nguyen <Fluffywin@gmail.com>
2021-10-18 18:27:28 -0400

    new project

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LiftComponent } from './lift/lift.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LiftComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
