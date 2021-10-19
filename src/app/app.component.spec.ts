c9ba439f0109f67bb4fda0cf152af6232d49b8f5
Vi Nguyen <Fluffywin@gmail.com>
2021-10-18 20:21:54 -0400

    implementing component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-portfolio'`, () => {
+  it(`should have as title 'BuffBeefBoys'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('my-portfolio');
